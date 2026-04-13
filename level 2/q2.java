8df0c743f091c452d871c21b8041f35ae9cccbfc
SamriddhaDas <159179023+SamriddhaDas@users.noreply.github.com>
2026-04-13 14:40:02 +0530

    Create q2.java

---
+++ level 2/q2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        // Input
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+
+            System.out.print("Enter height of " + names[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        // Find youngest and tallest
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Output
+        System.out.println("Youngest friend is " + names[youngestIndex]);
+        System.out.println("Tallest friend is " + names[tallestIndex]);
+
+        sc.close();
+    }
+}
